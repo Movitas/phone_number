860e28b08aaf022b83c885a1571e6b38592e7999
Justin Campbell <jcampbell@movitas.com>
2011-06-29 15:56:02 -0400

    Require Number class

--- lib/phone_number.rb
+++ lib/phone_number.rb
@@ -2,3 +2,4 @@ $:.unshift(File.dirname(__FILE__)) unless
   $:.include?(File.dirname(__FILE__)) || $:.include?(File.expand_path(File.dirname(__FILE__)))
 
 require 'phone_number/metadata'
+require 'phone_number/number'
