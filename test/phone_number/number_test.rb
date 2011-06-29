813acd238160cd2f48f6fe6a56ed4c697e99e228
Justin Campbell <jcampbell@movitas.com>
2011-06-29 17:03:11 -0400

    Number tests

---
+++ test/phone_number/number_test.rb
@@ -0,0 +1,37 @@
+require File.expand_path(File.dirname(__FILE__) + '/../test_helper')
+
+class NumberTest < Test::Unit::TestCase
+  
+  def setup
+    @valid_number              = PhoneNumber::Number.new "+12155551212"
+    @valid_number_with_plus    = PhoneNumber::Number.new "+12155551212"
+    @valid_number_without_plus = PhoneNumber::Number.new "12155551212"
+    @invalid_number_too_short  = PhoneNumber::Number.new "12"
+    @invalid_number_too_long   = PhoneNumber::Number.new "12345678901234567890"
+  end
+  
+  test "should require an argument" do
+    assert_raise(ArgumentError) { PhoneNumber::Number.new }
+  end
+  
+  test "should instantiate with a valid phone number" do
+    assert @valid_number
+  end
+  
+  test "should return dialed number when to_s is called" do
+    assert_equal "+12155551212", @valid_number.to_s
+  end
+  
+  test "should determine if a number is viable by length and regex" do
+    assert @valid_number_with_plus   .is_viable?
+    assert @valid_number_without_plus.is_viable?
+    assert !@invalid_number_too_long .is_viable?
+    assert !@invalid_number_too_short.is_viable?
+  end
+  
+  test "is_viable class method" do
+    assert  PhoneNumber::Number.is_viable?("12155551212")
+    assert !PhoneNumber::Number.is_viable?("12")
+  end
+  
+end
