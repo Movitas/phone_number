2b30adbb54a571c0608f7921dc18cc9fa247aea2
Justin Campbell <jcampbell@movitas.com>
2011-06-29 11:39:53 -0400

    Remove lib/ from require path

--- lib/phone_number.rb
+++ lib/phone_number.rb
@@ -1,1 +1,4 @@
-require 'lib/phone_number/metadata'
+$:.unshift(File.dirname(__FILE__)) unless
+  $:.include?(File.dirname(__FILE__)) || $:.include?(File.expand_path(File.dirname(__FILE__)))
+
+require 'phone_number/metadata'
